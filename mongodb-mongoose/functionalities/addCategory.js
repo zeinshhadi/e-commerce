b05ec450c99923dc7d240f132026e384df804281
HaninKodamy <haneenkodamy200@gmail.com>
2023-09-16 14:11:32 +0300

    zein

--- mongodb-mongoose/functionalities/addCategory.js
+++ mongodb-mongoose/functionalities/addCategory.js
@@ -1,18 +1,62 @@
-import Category from '../models/Category.js';
-import "../connectDB.js";
+import mongoose from "mongoose";
+import Category from "../models/Category.js"; // Import the Category model
+import '../connectDB.js';
 
-const category1= await Category.create({
-    name: "hanin",
+// Create categories for your e-commerce website
+const clothingCategory = new Category({
+  name: "Clothing",
 });
 
-console.log(category1);
-await category1
-  .save()
-  .then(() => {
-    console.log("category saved 2");
-  })
-  .catch((error) => {
-    console.error("error", error);
-  });
-  const firstCategory = await Category.findOne({});
-  console.log(firstCategory);
+const womensClothingCategory = new Category({
+  name: "Women's Clothing",
+  parentCategory: clothingCategory._id, // Set the parent category
+});
+
+const mensClothingCategory = new Category({
+  name: "Men's Clothing",
+  parentCategory: clothingCategory._id, // Set the parent category
+});
+
+const mobileAccessoriesCategory = new Category({
+  name: "Mobile & Accessories",
+});
+
+const mobilePhonesCategory = new Category({
+  name: "Mobile Phones",
+  parentCategory: mobileAccessoriesCategory._id, // Set the parent category
+});
+
+const mobileAccessoriesSubCategory = new Category({
+  name: "Mobile Accessories",
+  parentCategory: mobileAccessoriesCategory._id, // Set the parent category
+});
+
+const vehicleCategory = new Category({
+  name: "Vehicles",
+});
+
+const carsForSaleCategory = new Category({
+  name: "Cars for Sale",
+  parentCategory: vehicleCategory._id, // Set the parent category
+});
+
+// Save the categories to the database
+(async () => {
+  try {
+    await clothingCategory.save();
+    await womensClothingCategory.save();
+    await mensClothingCategory.save();
+    await mobileAccessoriesCategory.save();
+    await mobilePhonesCategory.save();
+    await mobileAccessoriesSubCategory.save();
+    await vehicleCategory.save();
+    await carsForSaleCategory.save();
+
+    console.log("Categories added successfully.");
+  } catch (error) {
+    console.error("Error adding categories:", error);
+  } finally {
+    mongoose.disconnect();
+  }
+})();
+
