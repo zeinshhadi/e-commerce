313b37f4d95e73e579845181d583c69a085e6b4f
Zein Shhadi <zeinshhadi@hotmail.com>
2023-09-16 12:37:23 +0300

    trash

--- mongodb-mongoose/models/order.js
+++ mongodb-mongoose/models/order.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
-const User = require("./models/user.js");
+const User = require("./models/User");
 const orderItemSchema = new mongoose.Schema({
   listing: {
     type: mongoose.Schema.Types.ObjectId,
